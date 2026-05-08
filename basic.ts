a05976c4a37e2a40877223a1cfc08a439672e96e
MarsalShyam <shyam12marsal@gmail.com>
2026-05-08 17:25:27 +0530

    basic ts code

---
+++ basic.ts
@@ -0,0 +1,8 @@
+function greet(name:string):string{
+    return `Hello ${name}`;
+}
+
+console.log(greet("shyam"));
+
+// console.log(greet(43)); //IT show error, return type number is not allowed
+
