2fa3b60113934e08cfae4fa4ec1972de8d349e58
MarsalShyam <shyam12marsal@gmail.com>
2026-05-08 18:42:13 +0530

    typesinTs

---
+++ TS_setup/src/typesinTs.ts
@@ -0,0 +1,14 @@
+//Inference Example
+let drink="tea";
+let cups=Math.random()>0.5?10:5;
+
+//Type error
+let name="Ganesh";
+// name=123;  //It is type error
+
+
+//Anotate code
+let Sname:string="shyam";
+Sname="Ganesh"
+let Roll:number=123;
+let Student:boolean=true;
