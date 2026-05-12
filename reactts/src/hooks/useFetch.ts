af3ef16c58c2c24736f7d3b81a8d4bb8364279e5
MarsalShyam <shyam12marsal@gmail.com>
2026-05-12 21:16:41 +0530

    commiting

---
+++ reactts/src/hooks/useFetch.ts
@@ -0,0 +1,26 @@
+// Creating custome hook for the fetch
+
+import {useEffect, useState} from "react"
+
+interface FetchState<T>{
+    data:T|null;
+    loading:boolean;
+    error:string|null
+}
+
+export function useFetch<T>(url:string):FetchState<T>{
+    const [state, setState]=useState<FetchState<T>>({
+        data:null,
+        loading:true,
+        error:null,
+    })
+
+
+    //useEffect to make fetch request
+
+
+
+    
+
+    return state
+}
